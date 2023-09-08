c5756082a9b52d3b445e457764f2c9f63e52cf56
Youssef Ouarad <youssef.ouarad@polymtl.ca>
2023-09-08 00:16:09 -0400

    ajout du squelette de la vue de la liste de jeux a partir dun fichier json tampon

--- client/src/app/services/communication.service.ts
+++ client/src/app/services/communication.service.ts
@@ -4,15 +4,20 @@ import { Message } from '@common/message';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { Game } from '@app/interfaces/games';
 
 @Injectable({
     providedIn: 'root',
 })
 export class CommunicationService {
     private readonly baseUrl: string = environment.serverUrl;
+    private gamesUrl = '../assets/example-list-of-games.json'; // fichier test pour generer une liste de jeux
 
     constructor(private readonly http: HttpClient) {}
 
+    getGames(): Observable<Game[]> {
+        return this.http.get<Game[]>(this.gamesUrl);
+    }
     basicGet(): Observable<Message> {
         return this.http.get<Message>(`${this.baseUrl}/example`).pipe(catchError(this.handleError<Message>('basicGet')));
     }
