e5a79679fc25e9b634c2aca9a866199a51c81955
Bryan-Tad <bryan.tadie@polymtl.ca>
2023-09-11 20:30:55 -0400

    squelette du chat ajouté et dynamisme de l'interface amélioré

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ChatComponent } from '@app/components/chat/chat.component';
 import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
+import { QuestionZoneComponent } from '@app/components/question-zone/question-zone.component';
 import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
 import { AppRoutingModule } from '@app/modules/app-routing.module';
 import { AppMaterialModule } from '@app/modules/material.module';
@@ -12,8 +13,7 @@ import { AppComponent } from '@app/pages/app/app.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
-import { RealGamePageComponent } from './pages/real-game-page/real-game-page.component';
-import { QuestionZoneComponent } from './components/question-zone/question-zone.component';
+import { RealGamePageComponent } from '@app/pages/real-game-page/real-game-page.component';
 
 /**
  * Main module that is used in main.ts.
