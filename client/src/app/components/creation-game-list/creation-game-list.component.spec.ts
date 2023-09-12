a49859a12ae12276b6fddb511dfb02664b8d6ae9
RimaAz <90001216+RimaAz@users.noreply.github.com>
2023-09-11 23:02:21 -0400

    ajout de tests pour la composante creation-game-list

--- client/src/app/components/creation-game-list/creation-game-list.component.spec.ts
+++ client/src/app/components/creation-game-list/creation-game-list.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CreationGameListComponent } from './creation-game-list.component';
 
 describe('CreationGameListComponent', () => {
@@ -17,5 +16,95 @@ describe('CreationGameListComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should toggle game details', () => {
+        component.toggleDetails(0);
+        expect(component.selectedGameIndex).toBe(0);
+
+        component.toggleDetails(1);
+        expect(component.selectedGameIndex).toBe(1);
+
+        component.toggleDetails(1);
+        expect(component.selectedGameIndex).toBe(null);
+    });
+
+    it('should only show visible games', () => {
+        const visibleGame = {
+            name: 'visible',
+            dateCreated: '11-11-11',
+            visible: true,
+            description: 'description jeu1',
+            time: '11:11',
+            questions: [],
+        };
+        const notVisibleGame = {
+            name: 'invisible',
+            dateCreated: '11-11-11',
+            visible: false,
+            description: 'description jeu1',
+            time: '11:11',
+            questions: [],
+        };
+
+        component.games = [visibleGame, notVisibleGame];
+        const visibleGames = component.getVisibleGames();
+
+        expect(visibleGames.length).toEqual(1);
+        expect(visibleGames[0]).toEqual(visibleGame);
+    });
+
+    it('should get questions', () => {
+        const gameWithQuestions = {
+            name: 'test',
+            dateCreated: '11-11-11',
+            visible: true,
+            description: 'description jeu1',
+            time: '11:11',
+            questions: [
+                {
+                    type: 'QCM',
+                    text: 'Question 1',
+                    choices: [
+                        { text: 'Choice 1', isCorrect: false },
+                        { text: 'Choice 2', isCorrect: true },
+                    ],
+                },
+                {
+                    type: 'QCM',
+                    text: 'Question 2',
+                    choices: [
+                        { text: 'Choice 1', isCorrect: true },
+                        { text: 'Choice 2', isCorrect: false },
+                    ],
+                },
+            ],
+        };
+
+        const gameWithoutQuestions = {
+            name: 'test',
+            dateCreated: '11-11-11',
+            visible: true,
+            description: 'description jeu1',
+            time: '11:11',
+            questions: [],
+        };
+
+        const noQuestions = component.getQuestionsArray(gameWithoutQuestions);
+        const hasQuestions = component.getQuestionsArray(gameWithQuestions);
+
+        expect(noQuestions).toBeNull();
+        expect(noQuestions.length).toEqual(0);
+        expect(hasQuestions.length).toEqual(2);
+        expect(hasQuestions[0].text).toBe('Question 1');
+        expect(hasQuestions[1].text).toBe('Question 2');
+    });
+
+    it('should redirect to testing page', () => {
+        // à compléter
+    });
+
+    it('should redirect to play page', () => {
+        // à compléter
     });
 });
