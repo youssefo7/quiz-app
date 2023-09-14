269acdb06a367dfca553fc26350aa865ff3b2921
Youssef Ouarad <youssef.ouarad@polymtl.ca>
2023-09-14 14:50:30 -0400

    squelette de la vue admin version final

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,7 +11,6 @@ import { AppComponent } from '@app/pages/app/app.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
-import { GameListComponent } from './components/game-list/game-list.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 import { HostGamePageComponent } from './pages/host-game-page/host-game-page.component';
