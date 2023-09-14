c3bec198f96085f0cb2232dcf4d1ab694a364c34
Youssef Ouarad <youssef.ouarad@polymtl.ca>
2023-09-14 14:50:30 -0400

    squelette de la vue admin version final

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -12,11 +12,11 @@ import { AppComponent } from '@app/pages/app/app.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
-import { GameListComponent } from './components/game-list/game-list.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 import { HostGamePageComponent } from './pages/host-game-page/host-game-page.component';
 import { AdminPopupComponent } from './components/admin-popup/admin-popup.component';
+import { QuizListComponent } from './components/quiz-list/quiz-list.component';
 
 /**
  * Main module that is used in main.ts.
@@ -35,7 +35,7 @@ import { AdminPopupComponent } from './components/admin-popup/admin-popup.compon
         AdminPageComponent,
         HostGamePageComponent,
         TopBarComponent,
-        GameListComponent,
+        QuizListComponent,
         AdminPopupComponent,
     ],
     imports: [AppMaterialModule, AppRoutingModule, BrowserAnimationsModule, BrowserModule, FormsModule, HttpClientModule, MatDialogModule],
