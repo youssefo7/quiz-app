ec5d8c9fe523c0e9fd8e4d5b8f2d7387fe82b210
Youssef Ouarad <youssef.ouarad@polymtl.ca>
2023-09-14 14:23:26 -0400

    squelette de la vue admin DONE

--- client/src/app/components/game-list/game-list.component.ts
+++ client/src/app/components/game-list/game-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { CommunicationService } from '@app/services/communication.service';
-import { Game } from '@app/interfaces/games';
+//  import { Game } from '@app/interfaces/games';
+//  import { BehaviorSubject } from 'rxjs';
 
 @Component({
     selector: 'app-game-list',
@@ -8,33 +8,19 @@ import { Game } from '@app/interfaces/games';
     styleUrls: ['./game-list.component.scss'],
 })
 export class GameListComponent {
-    games: Game[];
-
-    constructor(private gameService: CommunicationService) {
-        this.getGames();
-    }
-
-    getGames(): void {
-        this.gameService.getGames().subscribe((games) => {
-            this.games = games;
-        });
-    }
-
-    deleteGame(game: Game): void {
-        game.name = 'test';
-    }
-
-    exportGame(game: Game): void {
-        game.name = 'test 2';
-    }
-
-    editGame(game: Game): void {
-        // Implement logic to navigate to the edit page for the game
-        game.name = 'test 3';
-    }
-
-    toggleVisibility(game: Game): void {
-        game.visible = !game.visible;
-        game.name = 'test 4';
-    }
+    //  gameList: BehaviorSubject<Quiz[]> = new BehaviorSubject<Quiz[]>([]);
+    // These functions are yet to be implemented, the code inside is just filler to push a compilable code
+    // deleteGame(game: Game): void {
+    //     game.name = 'game 1';
+    // }
+    // exportGame(game: Game): void {
+    //     game.name = 'game 2';
+    // }
+    // editGame(game: Game): void {
+    //     game.name = 'game 3';
+    // }
+    // toggleVisibility(game: Game): void {
+    //     game.visible = !game.visible;
+    //     game.name = 'game 4';
+    // }
 }
