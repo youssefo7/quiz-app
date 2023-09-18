48968802486570e0d15ea83d73ae9b18b4761dc5
RimaAz <90001216+RimaAz@users.noreply.github.com>
2023-09-17 20:50:45 -0400

    communication serveur pour recuperer les quiz

--- client/src/app/services/communication.service.ts
+++ client/src/app/services/communication.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Quiz } from '@app/interfaces/quiz';
 import { Message } from '@common/message';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
@@ -10,6 +11,7 @@ import { environment } from 'src/environments/environment';
 })
 export class CommunicationService {
     private readonly baseUrl: string = environment.serverUrl;
+    private quizzesURL = '../assets/quizzes.json'; // fichier test pour generer une liste de jeux
 
     constructor(private readonly http: HttpClient) {}
 
@@ -19,6 +21,14 @@ export class CommunicationService {
 
     basicPost(message: Message): Observable<HttpResponse<string>> {
         return this.http.post(`${this.baseUrl}/example/send`, message, { observe: 'response', responseType: 'text' });
+    }
+
+    getQuizzes(): Observable<Quiz[]> {
+        return this.http.get<Quiz[]>(this.quizzesURL);
+    }
+
+    getQuiz(id: string): Observable<Quiz> {
+        return this.http.get<Quiz>(`${this.baseUrl}/quizzes/${id}`).pipe(catchError(this.handleError<Quiz>('getQuiz')));
     }
 
     private handleError<T>(request: string, result?: T): (error: Error) => Observable<T> {
