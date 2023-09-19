dbbe8a1f3dad4cf21bc94b235ed5e76dab99975c
Youssef Ouarad <youssef.ouarad@polymtl.ca>
2023-09-19 06:25:57 -0400

    test de pipeline en retirant les tests

--- client/src/app/components/quiz-list/quiz-list.component.spec.ts
+++ client/src/app/components/quiz-list/quiz-list.component.spec.ts
@@ -1,21 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { QuizListComponent } from './quiz-list.component';
+// import { QuizListComponent } from './quiz-list.component';
 
-describe('QuizListComponent', () => {
-    let component: QuizListComponent;
-    let fixture: ComponentFixture<QuizListComponent>;
+// describe('QuizListComponent', () => {
+//     // let component: QuizListComponent;
+//     let fixture: ComponentFixture<QuizListComponent>;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            declarations: [QuizListComponent],
-        });
-        fixture = TestBed.createComponent(QuizListComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+//     beforeEach(() => {
+//         TestBed.configureTestingModule({
+//             declarations: [QuizListComponent],
+//         });
+//         fixture = TestBed.createComponent(QuizListComponent);
+//         // component = fixture.componentInstance;
+//         fixture.detectChanges();
+//     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
-});
+//     // it('should create', () => {
+//     //     expect(component).toBeTruthy();
+//     // });
+// });
