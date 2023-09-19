b6e5bf5933b263859329f17a0fc62341771f6e79
Youssef Ouarad <youssef.ouarad@polymtl.ca>
2023-09-18 22:17:43 -0400

    ajout des fonctionnalités delete, export, toggle visibilité

--- client/src/app/components/quiz-list/quiz-list.component.ts
+++ client/src/app/components/quiz-list/quiz-list.component.ts
@@ -1,26 +1,65 @@
-import { Component } from '@angular/core';
-//  import { Game } from '@app/interfaces/games';
-//  import { BehaviorSubject } from 'rxjs';
+import { CommunicationService } from '@app/services/communication.service';
+import { Component, OnInit } from '@angular/core';
+import { Quiz } from '@app/interfaces/quiz';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
     selector: 'app-quiz-list',
     templateUrl: './quiz-list.component.html',
     styleUrls: ['./quiz-list.component.scss'],
 })
-export class QuizListComponent {
-    //  gameList: BehaviorSubject<Quiz[]> = new BehaviorSubject<Quiz[]>([]);
-    // These functions are yet to be implemented, the code inside is just filler to push a compilable code
-    // deleteGame(game: Game): void {
-    //     game.name = 'game 1';
-    // }
-    // exportGame(game: Game): void {
-    //     game.name = 'game 2';
-    // }
-    // editGame(game: Game): void {
-    //     game.name = 'game 3';
-    // }
-    // toggleVisibility(game: Game): void {
-    //     game.visible = !game.visible;
-    //     game.name = 'game 4';
-    // }
+export class QuizListComponent implements OnInit {
+    quizList: BehaviorSubject<Quiz[]> = new BehaviorSubject<Quiz[]>([]);
+    quizzes: Quiz[] = [];
+
+    constructor(private communicationService: CommunicationService) {}
+
+    ngOnInit(): void {
+        this.fetchQuizzes();
+    }
+
+    fetchQuizzes(): void {
+        this.communicationService.getQuizzes().subscribe((quizzes) => {
+            this.quizList.next(quizzes);
+            this.quizzes = quizzes;
+        });
+    }
+
+    deleteQuiz(quiz: Quiz): void {
+        this.communicationService.deleteQuiz(quiz.id).subscribe(() => {
+            this.quizzes = this.quizzes.filter((q) => q !== quiz);
+        });
+    }
+
+    exportQuiz(quiz: Quiz): void {
+        this.communicationService.exportQuiz(quiz.id).subscribe({
+            next: (response) => {
+                const quizName: string = response.title;
+                //  create blob file
+                const blob = new Blob([JSON.stringify(response)], { type: 'application/json' });
+                const blobUrl = window.URL.createObjectURL(blob);
+                //  create anchor element (invisible in html)
+                const anchor = document.createElement('a');
+                anchor.href = blobUrl;
+                anchor.download = quizName;
+
+                anchor.click();
+
+                window.URL.revokeObjectURL(blobUrl);
+            },
+            error: (error) => {
+                throw new Error(`Error exporting quiz: ${error.message}`);
+            },
+        });
+    }
+
+    editQuiz(quiz: Quiz): void {
+        // Implement edit logic here
+        quiz.title = 'editing...';
+    }
+
+    toggleVisibility(quiz: Quiz): void {
+        quiz.visibility = !quiz.visibility;
+        this.communicationService.updateQuiz(quiz.id, quiz).subscribe();
+    }
 }
