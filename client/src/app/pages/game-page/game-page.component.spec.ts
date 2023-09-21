8a5cba2ea387ff3e58a9619476c69113de2a0c30
Bryan-Tad <bryan.tadie@polymtl.ca>
2023-09-21 08:17:24 -0400

    rebase with dev

--- client/src/app/pages/game-page/game-page.component.spec.ts
+++ client/src/app/pages/game-page/game-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material/icon';
 import { ActivatedRoute, RouterModule } from '@angular/router';
+import { CountdownComponent } from '@app/components/countdown/countdown.component';
 import { TopBarComponent } from '@app/components/top-bar/top-bar.component';
 import { CommunicationService } from '@app/services/communication.service';
 import { of } from 'rxjs';
@@ -28,7 +29,7 @@ describe('GamePageComponent', () => {
 
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
-            declarations: [GamePageComponent, TopBarComponent],
+            declarations: [GamePageComponent, TopBarComponent, CountdownComponent],
             imports: [MatIconModule, RouterModule],
             providers: [
                 { provide: CommunicationService, useValue: communicationServiceMock },
