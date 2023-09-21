99f6bb516ddb61e086fe6332388b502202843c32
alioa <ali-1.abbas@polymt.ca>
2023-09-21 13:17:06 -0400

    erreurs francais

--- server/app/services/quizzes/quizzes.service.ts
+++ server/app/services/quizzes/quizzes.service.ts
@@ -29,10 +29,10 @@ export class QuizzesService {
     verifyQuiz(quiz: Quiz): void {
         const errors = [];
 
-        if (!quiz.$schema || typeof quiz.$schema !== 'string') errors.push('Schéma de quiz invalide ou manquant');
+        if (!quiz.$schema || typeof quiz.$schema !== 'string') errors.push('Schéma du quiz invalide ou manquant');
         // not checking id here because it is generated in addQuiz()
         // if (!quiz.id || typeof quiz.id !== 'string') errors.push('id de quiz invalide ou manquant');
-        if (!quiz.title || typeof quiz.title !== 'string') errors.push('Titre de quiz invalide ou manquant');
+        if (!quiz.title || typeof quiz.title !== 'string') errors.push('Titre du quiz invalide ou manquant');
         if (!quiz.duration || typeof quiz.duration !== 'number') errors.push('La durée du quiz est manquante doit être un nombre');
         if (quiz.duration < Constants.MIN_DURATION || quiz.duration > Constants.MAX_DURATION)
             errors.push('La durée du quiz doit être entre 10 et 60 secondes');
