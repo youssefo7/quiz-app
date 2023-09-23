494c1eaecba6ae107569a6eb812f7928bd20e245
Bryan-Tad <bryan.tadie@polymtl.ca>
2023-09-23 00:37:24 -0400

    transfère de point implémenté

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -9,19 +9,24 @@ import { CommunicationService } from '@app/services/communication.service';
     styleUrls: ['./game-page.component.scss', '../../../assets/shared.scss'],
 })
 export class GamePageComponent implements OnInit {
-    title = 'Partie';
-    link = '/home';
+    title: string;
+    link: string;
     quiz: Quiz;
+    playerPoints: number;
     private readonly isTestGame = this.route.snapshot.url.some((segment) => segment.path === 'test');
 
     constructor(
         private communicationService: CommunicationService,
         private route: ActivatedRoute,
-    ) {}
+    ) {
+        this.title = 'Partie';
+        this.link = '/home';
+        this.playerPoints = 0;
+    }
 
     checkGameRoute(isTestGame = this.isTestGame) {
         if (isTestGame) {
-            this.link = '/game/new'; // TODO: Change with create-game route
+            this.link = '/game/new';
             this.title += ' - Test';
         }
     }
@@ -34,6 +39,10 @@ export class GamePageComponent implements OnInit {
                 this.quiz = quiz;
             });
         }
+    }
+
+    givePoints(points: number) {
+        this.playerPoints += points;
     }
 
     ngOnInit() {
