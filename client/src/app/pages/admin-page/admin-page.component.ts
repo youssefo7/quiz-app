5b35485cabec39ab0c09515ec85dac28f0bce121
Ali-Abbas01 <ali-1.abbas@polymtl.ca>
2023-09-29 23:57:11 +0000

    Demande du mot passe lors du refresh de la page admin

--- client/src/app/pages/admin-page/admin-page.component.ts
+++ client/src/app/pages/admin-page/admin-page.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { AdminGuardService } from '@app/services/admin-guard.service';
 
 @Component({
     selector: 'app-admin-page',
     templateUrl: './admin-page.component.html',
     styleUrls: ['./admin-page.component.scss', '../../../assets/shared.scss'],
 })
-export class AdminPageComponent {}
+export class AdminPageComponent implements OnInit {
+    constructor(
+        private router: Router,
+        private adminGuardService: AdminGuardService,
+    ) {
+        this.handleAdminPageExit();
+    }
+
+    ngOnInit(): void {
+        this.adminGuardService.pageRefreshState();
+    }
+
+    handleAdminPageExit(): void {
+        this.router.events.subscribe((event) => {
+            if (event instanceof NavigationEnd && event.url !== '/admin') {
+                sessionStorage.removeItem('isRefreshed');
+            }
+        });
+    }
+}
