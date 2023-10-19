4256b6e36dd024e931a6fdf82ab003430ce64669
Massimo Donato <massimo.donato@polymtl.ca>
2023-10-19 16:19:30 -0400

    Debut de la creation de la base pour Websockets

---
+++ server/app/services/socket-manager/socket-manager.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { SocketManagerService } from './socket-manager.service';
+
+describe('SocketManagerService', () => {
+  let service: SocketManagerService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [SocketManagerService],
+    }).compile();
+
+    service = module.get<SocketManagerService>(SocketManagerService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
