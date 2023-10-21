07b24d6a06e9d2dfe2c62e637be3894adfac46d7
AliAbbas10 <132827444+AliAbbas10@users.noreply.github.com>
2023-10-21 18:28:16 -0400

    changement url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://ec2-15-222-242-181.ca-central-1.compute.amazonaws.com:3000/api',
+    serverUrl: 'http://ec2-35-183-43-177.ca-central-1.compute.amazonaws.com:3000/api',
 };
