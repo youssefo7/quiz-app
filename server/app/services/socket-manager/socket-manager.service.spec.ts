a9bbcde85338998811b71ffada9f3ef31c7c763d
Massimo Donato <massimo.donato@polymtl.ca>
2023-10-25 10:17:47 -0400

    Ajout de certaines fonctionnalites pour la communication socket (brouillon)

--- server/app/services/socket-manager/socket-manager.service.spec.ts
+++ server/app/services/socket-manager/socket-manager.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { SocketManagerService } from './socket-manager.service';
 
 describe('SocketManagerService', () => {
-  let service: SocketManagerService;
+    let service: SocketManagerService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [SocketManagerService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [SocketManagerService],
+        }).compile();
 
-    service = module.get<SocketManagerService>(SocketManagerService);
-  });
+        service = module.get<SocketManagerService>(SocketManagerService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
