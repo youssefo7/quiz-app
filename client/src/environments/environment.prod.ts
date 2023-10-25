8d0b00161e401cfa7bf0068ce92ef04ff02489a3
AliAbbas10 <132827444+AliAbbas10@users.noreply.github.com>
2023-10-25 19:38:15 -0400

    nouvelle instance aws

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://ec2-35-183-43-177.ca-central-1.compute.amazonaws.com:3000/api',
+    serverUrl: 'http://ec2-3-99-158-111.ca-central-1.compute.amazonaws.com:3000/api',
 };
