dc2ddf5aa06db02d7a243de29caac08ccaa36b0c
Massimo Donato <massimo.donato@polymtl.ca>
2023-10-26 13:06:18 -0400

    Premiere version a tester

--- server/app/interfaces/room.ts
+++ server/app/interfaces/room.ts
@@ -1,18 +1,25 @@
 export interface Room {
     id: string;
     quizId: string;
-    organizer: User;
-    players: User[];
+    organizer: Organizer;
+    players: Player[];
     isLocked: boolean;
     bannedNames: string[];
-    abandonnedPlayers: string[];
     answerTimes: AnswerTime[];
+    bonusCount: number;
 }
 
 export interface User {
     socketId: string;
     name: string;
 }
+
+export interface Player extends User {
+    points: number;
+    hasAbandonned: boolean;
+}
+
+export interface Organizer extends User {}
 
 export interface AnswerTime {
     userId: string;
---
+++ server/app/services/room-manager/room-manager.service.ts
@@ -0,0 +1,126 @@
+import { AnswerTime, Organizer, Player, Room } from '@app/interfaces/room';
+import { Injectable } from '@nestjs/common';
+import * as randomstring from 'randomstring';
+
+@Injectable()
+export class RoomManagerService {
+    rooms: Room[] = [];
+
+    createRoomId() {
+        let roomId = '';
+        let roomExists: Room;
+
+        do {
+            roomId = randomstring.generate({
+                charset: 'numeric',
+                length: 4,
+            });
+            roomExists = this.findRoom(roomId);
+        } while (roomExists);
+
+        return roomId;
+    }
+
+    createNewRoom(quizId: string, organizerId: string) {
+        const roomId = this.createRoomId();
+        this.rooms.push({
+            id: roomId,
+            quizId,
+            organizer: { socketId: organizerId, name: 'Organisateur' },
+            players: [],
+            isLocked: false,
+            bannedNames: [],
+            // abandonnedPlayers: [],
+            answerTimes: [],
+            bonusCount: 0,
+        });
+
+        return roomId;
+    }
+
+    addPlayerToRoom(room: Room, playerId: string) {
+        room.players.push({ socketId: playerId, name: '', points: 0, hasAbandonned: false });
+    }
+
+    addBannedNameToRoom(room: Room, name: string) {
+        room.bannedNames.push(name);
+    }
+
+    addPointsToPlayer(roomId: string, name: string, points: number) {
+        const room = this.findRoom(roomId);
+        const player = this.findPlayerByName(room, name);
+        player.points += points;
+    }
+
+    findRoom(roomId: string): Room {
+        return this.rooms.find((room) => room.id === roomId);
+    }
+
+    findUser(id: string): Player | Organizer {
+        let organizers: Organizer[];
+
+        for (let i = 0; i < this.rooms.length; i++) {
+            organizers.push(this.rooms[i].organizer);
+            for (let j = 0; i < this.rooms[i].players.length; j++) {
+                if (this.rooms[i].players[j].socketId === id) {
+                    return this.rooms[i].players[j];
+                }
+            }
+        }
+
+        return organizers.find((organizer) => organizer.socketId === id);
+    }
+
+    findPlayerByName(room: Room, name: string) {
+        return room.players.find((user) => user.name === name);
+    }
+
+    checkName(room: Room, name: string) {
+        const nameExists = room.players.find((player) => player.name.toLowerCase() === name.toLowerCase());
+
+        if (name.toLowerCase() === 'organisateur') {
+            return true;
+        }
+        return nameExists ? true : false;
+    }
+
+    isBannedName(room: Room, name: string) {
+        const isBanned = room.bannedNames.find((bannedName) => bannedName === name);
+        return isBanned ? true : false;
+    }
+
+    removeUser(room: Room, userId: string) {
+        for (let i = 0; i < room.players.length; i++) {
+            if (room.players[i].socketId === userId) {
+                room.players.splice(i, 1);
+            }
+        }
+    }
+
+    deleteRoom(room: Room) {
+        for (let i = 0; i < this.rooms.length; i++) {
+            if (this.rooms[i].id === room.id) {
+                this.rooms.splice(i, 1);
+            }
+        }
+    }
+
+    getQuickestTime(room: Room): AnswerTime | null {
+        const player = room.answerTimes.reduce((fastestPlayer, currentPlayer) => {
+            if (currentPlayer.timeStamp < fastestPlayer.timeStamp) {
+                return (fastestPlayer = currentPlayer);
+            }
+        }, room.answerTimes[0]);
+
+        const copyAnswerTimes = { ...room.answerTimes }.filter((newPlayer) => newPlayer.timeStamp === player.timeStamp);
+        if (copyAnswerTimes.length > 1) {
+            return null;
+        }
+
+        return player;
+    }
+
+    resetTimes(room: Room) {
+        room.answerTimes.splice(0, room.answerTimes.length);
+    }
+}
