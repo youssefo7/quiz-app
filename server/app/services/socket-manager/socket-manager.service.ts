5134e30e974c1c53c1825329e9809b15a5267c1d
Massimo Donato <massimo.donato@polymtl.ca>
2023-10-26 13:06:18 -0400

    Premiere version a tester

--- server/app/services/socket-manager/socket-manager.service.ts
+++ server/app/services/socket-manager/socket-manager.service.ts
@@ -1,47 +1,41 @@
-import { AnswerTime, Room, User } from '@app/interfaces/room';
+import { Player, Room, User } from '@app/interfaces/room';
+import { RoomManagerService } from '@app/services/room-manager/room-manager.service';
 import { Injectable } from '@nestjs/common';
 import * as http from 'http';
-import * as randomstring from 'randomstring';
 import * as io from 'socket.io';
 
 @Injectable()
 export class SocketManagerService {
     private sio: io.Server;
-    private rooms: Room[] = [];
+    private rooms: Room[];
 
-    constructor(server: http.Server) {
+    constructor(
+        private server: http.Server,
+        private roomManager: RoomManagerService,
+    ) {
         this.sio = new io.Server(server, { cors: { origin: '*', methods: ['GET', 'POST'] } });
+        this.rooms = roomManager.rooms;
     }
 
     handleSockets(): void {
         this.sio.on('connection', (socket) => {
             // to create a new room
             socket.on('createRoom', (quizId: string, callback: (roomId: string) => void) => {
-                const roomId = this.createRoomId();
-                this.rooms.push({
-                    id: roomId,
-                    quizId,
-                    organizer: { socketId: socket.id, name: 'Organisateur' },
-                    players: [],
-                    isLocked: false,
-                    bannedNames: [],
-                    abandonnedPlayers: [],
-                    answerTimes: [],
-                });
+                const roomId = this.roomManager.createNewRoom(quizId, socket.id);
                 callback(roomId);
             });
 
             socket.on('toggleLockRoom', (roomId: string) => {
-                const room = this.findRoom(roomId);
+                const room = this.roomManager.findRoom(roomId);
                 room.isLocked = !room.isLocked;
             });
 
             // to join an existing room
             socket.on('joinRoom', (roomId: string, callback: (roomState: 'IS_LOCKED' | 'INVALID' | 'OK', quizId: string | null) => void) => {
-                const roomExists = this.findRoom(roomId);
+                const roomExists = this.roomManager.findRoom(roomId);
                 if (roomExists) {
                     if (!roomExists.isLocked) {
-                        roomExists.players.push({ socketId: socket.id, name: '' });
+                        this.roomManager.addPlayerToRoom(roomExists, socket.id);
                         socket.join(roomId);
                         callback('OK', roomExists.quizId);
                     } else {
@@ -52,6 +46,7 @@ export class SocketManagerService {
                 }
             });
 
+            // double check when player leaves before choosing name
             socket.on('chooseName', (name: string, callback: (validName: boolean) => void) => {
                 let wantedPlayer: User;
                 const playerRoom = this.rooms.find((room) => {
@@ -59,12 +54,18 @@ export class SocketManagerService {
                     return wantedPlayer ? true : false;
                 });
 
-                const nameExists = this.checkName(playerRoom, name);
-                const bannedName = this.isBannedName(playerRoom, name);
+                const nameExists = this.roomManager.checkName(playerRoom, name);
+                const bannedName = this.roomManager.isBannedName(playerRoom, name);
                 if (!nameExists && !bannedName) {
                     wantedPlayer.name = name;
                 }
-                callback(!nameExists && !bannedName ? true : false);
+
+                callback(!nameExists && !bannedName);
+            });
+
+            socket.on('successfulJoin', (data: { roomId: string; name: string }) => {
+                const room = this.roomManager.findRoom(data.roomId);
+                socket.to(room.id).emit('playerHasJoined', data.name);
             });
 
             // allows game to begin
@@ -73,42 +74,46 @@ export class SocketManagerService {
             });
 
             socket.on('getPlayerNames', (roomId: string, callback: (playerNames: string[]) => void) => {
-                const room = this.findRoom(roomId);
+                const room = this.roomManager.findRoom(roomId);
                 const playerNames = room.players.map((player) => player.name);
                 callback(playerNames);
             });
 
-            socket.on('banName', (data: { roomId: string; user: User }) => {
-                const room = this.findRoom(data.roomId);
-                room.bannedNames.push(data.user.name);
-                this.removeUser(room, data.user.name);
+            socket.on('banName', (data: { roomId: string; name: string }) => {
+                const room = this.roomManager.findRoom(data.roomId);
+                this.roomManager.addBannedNameToRoom(room, data.name);
+                const player = this.roomManager.findPlayerByName(room, data.name);
+                this.roomManager.removeUser(room, player.socketId);
             });
 
-            socket.on('playerLeaveGame', (data: { roomId: string; gameHasStarted: boolean }) => {
-                const user = this.findUser(socket.id);
-                const room = this.findRoom(data.roomId);
+            socket.on('playerLeaveGame', (data: { roomId: string; hasGameStarted: boolean }) => {
+                const user = this.roomManager.findUser(socket.id);
+                const room = this.roomManager.findRoom(data.roomId);
 
-                if (data.gameHasStarted) {
-                    room.abandonnedPlayers.push(user.name);
+                const isPlayer = user.name !== room.organizer.name;
+
+                if (data.hasGameStarted && isPlayer) {
+                    (user as Player).hasAbandonned = true;
                 }
-                this.removeUser(room, user.socketId);
+                this.roomManager.removeUser(room, user.socketId);
+                socket.to(room.organizer.socketId).emit('AbandonnedGame', user.name);
                 socket.leave(room.id);
             });
 
             // Use this function at the end of the game in order to kick everyone from game and delete it
             // case for : 1 ) Organizer leaves game, 2 ) End game button
             socket.on('endGame', async (roomId: string) => {
-                const room = this.findRoom(roomId);
+                const room = this.roomManager.findRoom(roomId);
                 const sockets = await this.sio.sockets.fetchSockets();
 
                 socket.to(room.id).emit('gameEnded');
                 sockets.forEach((playerSocket) => playerSocket.leave(room.id));
-                this.deleteRoom(room);
+                this.roomManager.deleteRoom(room);
             });
 
             // clavardage
             socket.on('roomMessage', (data: { message: string; roomId: string }) => {
-                const user = this.findUser(socket.id);
+                const user = this.roomManager.findUser(socket.id);
                 const time = new Date();
 
                 const timeString = time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds();
@@ -116,125 +121,54 @@ export class SocketManagerService {
             });
 
             socket.on('roomData', (roomId: string, callback: (roomData: Room) => void) => {
-                const room = this.findRoom(roomId);
+                const room = this.roomManager.findRoom(roomId);
                 callback(room);
             });
 
             socket.on('goodAnswer', (data: { roomId: string; timeStamp: Date }) => {
-                const room = this.findRoom(data.roomId);
+                const room = this.roomManager.findRoom(data.roomId);
                 room.answerTimes.push({ userId: socket.id, timeStamp: data.timeStamp.getTime() });
             });
 
             socket.on('questionChoiceClick', (data: { roomId: string; questionChoiceIndex: number }) => {
-                const organizer = this.findRoom(data.roomId).organizer.socketId;
+                const organizer = this.roomManager.findRoom(data.roomId).organizer.socketId;
                 this.sio.to(organizer).emit('addQuestionChoice', data.questionChoiceIndex);
             });
 
             socket.on('questionChoiceUnselect', (data: { roomId: string; questionChoiceIndex: number }) => {
-                const organizer = this.findRoom(data.roomId).organizer.socketId;
+                const organizer = this.roomManager.findRoom(data.roomId).organizer.socketId;
                 this.sio.to(organizer).emit('removeQuestionChoice', data.questionChoiceIndex);
             });
 
             socket.on('giveBonus', (roomId: string) => {
-                const quickestPlayer = this.getQuickestTime(this.findRoom(roomId));
+                const quickestPlayer = this.roomManager.getQuickestTime(this.roomManager.findRoom(roomId));
+                const room = this.roomManager.findRoom(roomId);
                 if (quickestPlayer) {
+                    room.bonusCount++;
                     this.sio.to(quickestPlayer.userId).emit('bonus');
                 }
             });
 
-            socket.on('nextQuestion', async (roomId: string) => {
+            socket.on('addPointsToPlayer', (data: { roomId: string; name: string; points: number }, callback: (points: number) => void) => {
+                const room = this.roomManager.findRoom(data.roomId);
+                this.roomManager.addPointsToPlayer(data.roomId, data.name, data.points);
+                const player = this.roomManager.findPlayerByName(room, data.name);
+                callback(player.points);
+            });
+
+            socket.on('nextQuestion', (roomId: string) => {
+                const room = this.roomManager.findRoom(roomId);
+                this.roomManager.resetTimes(room);
                 socket.to(roomId).emit('nextQuestion');
             });
 
             // socket.on('disconnect', () => {
             //     const user = this.findUser(socket.id);
-            //     const room = this.findUserRoom(user);
 
             //     room.abandonnedPlayers.push(user.name);
             //     socket.leave(room.id);
             //     this.removeUser(room, user.name);
             // });
         });
-    }
-
-    /// ///////////////////////////////////////////////////////////////
-    // ADDITIONAL SUPPORT FUNCTIONS
-    /// ///////////////////////////////////////////////////////////////
-    private createRoomId() {
-        let roomId = '';
-        let roomExists: Room;
-
-        do {
-            roomId = randomstring.generate({
-                charset: 'numeric',
-                length: 4,
-            });
-            roomExists = this.findRoom(roomId);
-        } while (roomExists);
-
-        return roomId;
-    }
-
-    private findRoom(roomId: string): Room {
-        return this.rooms.find((room) => room.id === roomId);
-    }
-
-    private findUser(id: string): User {
-        let organizers: User[];
-
-        for (let i = 0; i < this.rooms.length; i++) {
-            organizers.push(this.rooms[i].organizer);
-            for (let j = 0; i < this.rooms[i].players.length; j++) {
-                if (this.rooms[i].players[j].socketId === id) {
-                    return this.rooms[i].players[j];
-                }
-            }
-        }
-
-        return organizers.find((organizer) => organizer.socketId === id);
-    }
-
-    private checkName(room: Room, name: string) {
-        const nameExists = room.players.find((player) => player.name.toLowerCase() === name.toLowerCase());
-        if (name.toLowerCase() === 'organisateur') {
-            return true;
-        }
-        return nameExists ? true : false;
-    }
-
-    private isBannedName(room: Room, name: string) {
-        const isBanned = room.bannedNames.find((bannedName) => bannedName === name);
-        return isBanned ? true : false;
-    }
-
-    private removeUser(room: Room, userId: string) {
-        for (let i = 0; i < room.players.length; i++) {
-            if (room.players[i].socketId === userId) {
-                room.players.splice(i, 1);
-            }
-        }
-    }
-
-    private deleteRoom(room: Room) {
-        for (let i = 0; i < this.rooms.length; i++) {
-            if (this.rooms[i].id === room.id) {
-                this.rooms.splice(i, 1);
-            }
-        }
-    }
-
-    private getQuickestTime(room: Room): AnswerTime | null {
-        const player = room.answerTimes.reduce((fastestPlayer, currentPlayer) => {
-            if (currentPlayer.timeStamp < fastestPlayer.timeStamp) {
-                return (fastestPlayer = currentPlayer);
-            }
-        }, room.answerTimes[0]);
-
-        const copyAnswerTimes = { ...room.answerTimes }.filter((newPlayer) => newPlayer.timeStamp === player.timeStamp);
-        if (copyAnswerTimes.length > 1) {
-            return null;
-        }
-
-        return player;
     }
 }
