cee8d796dfa409f5d8e3c022d2a069996b1f24ce
RimaAz <90001216+RimaAz@users.noreply.github.com>
2023-10-26 16:49:49 -0400

    ajout des bouttons, des popUps et de la liste de joueurs banis

--- client/src/app/components/player-list/player-list.component.ts
+++ client/src/app/components/player-list/player-list.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { PopupMessageComponent } from '@app/components/popup-message/popup-message.component';
+import { PopupMessageConfig } from '@app/interfaces/popup-message-config';
 
 @Component({
     selector: 'app-player-list',
@@ -7,9 +11,31 @@ import { Component } from '@angular/core';
 })
 export class PlayerListComponent {
     // Juste pour tester l'affichage
-    players: string[] = ['Joueur1', 'Joueur2', 'Joueur3', 'jouteur4', 'Joueur5', 'Joueur6', 'Joueur7', 'Joueur8', 'Joueur9', 'Joueur10', 'Joueur11'];
+    players: string[] = [
+        'Joueur1',
+        'Joueur2',
+        'Joueur3',
+        'joueur4',
+        'Joueur5',
+        'Joueur6',
+        'Joueur7',
+        'Joueur8',
+        'Joueur9',
+        'Joueur10',
+        'Joueur11',
+        'Joueur12',
+        'Joueur13',
+    ];
+
+    bannedPlayers: string[] = ['test1', 'test1', 'test1', 'test1', 'test1', 'test1', 'test1'];
+
     isHost = true;
-    isLocked = false;
+    isLocked: boolean;
+
+    constructor(
+        private router: Router,
+        private popUp: MatDialog,
+    ) {}
 
     lockGame() {
         this.isLocked = true;
@@ -19,6 +45,7 @@ export class PlayerListComponent {
         this.isLocked = false;
     }
 
+    // À enlever (juste pour tester)
     removePlayer(bannedPlayer: string) {
         this.players = this.players.reduce<string[]>((updatedList, player) => {
             if (player !== bannedPlayer) {
@@ -26,5 +53,51 @@ export class PlayerListComponent {
             }
             return updatedList;
         }, []);
+    }
+
+    startGame(): void {
+        if (!this.isHost) {
+            // naviguer vers: /game/:quizId/room/:roomId/
+            this.router.navigate([]);
+        } else {
+            // naviguer vers: /game/:quizId/room/:roomId/host
+            this.router.navigate([]);
+        }
+    }
+
+    quitPopUp(): void {
+        if (!this.isHost) {
+            this.playerQuitPopup();
+        } else {
+            this.hostQuitPopup();
+        }
+    }
+
+    hostQuitPopup(): void {
+        const config: PopupMessageConfig = {
+            message: 'Êtes-vous sur de vouloir annuler la partie?',
+            hasCancelButton: true,
+            okButtonText: 'Quitter',
+            okButtonFunction: () => {
+                this.router.navigate(['home/']);
+            },
+        };
+        const dialogRef = this.popUp.open(PopupMessageComponent);
+        const popupInstance = dialogRef.componentInstance;
+        popupInstance.config = config;
+    }
+
+    playerQuitPopup(): void {
+        const config: PopupMessageConfig = {
+            message: 'Êtes-vous sur de vouloir quitter?',
+            hasCancelButton: true,
+            okButtonText: 'Quitter',
+            okButtonFunction: () => {
+                this.router.navigate(['home/']);
+            },
+        };
+        const dialogRef = this.popUp.open(PopupMessageComponent);
+        const popupInstance = dialogRef.componentInstance;
+        popupInstance.config = config;
     }
 }
