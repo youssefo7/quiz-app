1359017ad7e34ac80219e563161cc9d315c87d1a
Youssef Ouarad <youssef.ouarad@polymtl.ca>
2023-10-26 11:54:19 -0400

    ajout du template de la liste de joueurs dans la vue de l'organisateur

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -27,6 +27,7 @@ import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 import { CreateEditQuizPageComponent } from './pages/create-edit-quiz-page/create-edit-quiz-page.component';
 import { CreateGamePageComponent } from './pages/create-game-page/create-game-page.component';
 import { WaitingPageComponent } from './pages/waiting-page/waiting-page.component';
+import { GamePlayersListComponent } from './components/game-players-list/game-players-list.component';
 
 /**
  * Main module that is used in main.ts.
@@ -57,6 +58,7 @@ import { WaitingPageComponent } from './pages/waiting-page/waiting-page.componen
         RangeValidatorDirective,
         TitleExistsDirective,
         CreateEditQuizPageComponent,
+        GamePlayersListComponent,
     ],
     imports: [
         AppMaterialModule,
---
+++ client/src/app/interfaces/player-info.ts
@@ -0,0 +1,5 @@
+export interface PlayerInfo {
+    name: string;
+    score: number;
+    hasAbandoned: boolean;
+}
