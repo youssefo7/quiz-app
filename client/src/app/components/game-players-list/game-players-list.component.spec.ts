2cbbe2e69053b4f7aebaf0d0ac6ee98995453b88
Youssef Ouarad <youssef.ouarad@polymtl.ca>
2023-10-26 12:36:00 -0400

    fix erreur lint

--- client/src/app/components/game-players-list/game-players-list.component.spec.ts
+++ client/src/app/components/game-players-list/game-players-list.component.spec.ts
@@ -3,19 +3,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GamePlayersListComponent } from './game-players-list.component';
 
 describe('GamePlayersListComponent', () => {
-  let component: GamePlayersListComponent;
-  let fixture: ComponentFixture<GamePlayersListComponent>;
+    let component: GamePlayersListComponent;
+    let fixture: ComponentFixture<GamePlayersListComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [GamePlayersListComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [GamePlayersListComponent],
+        });
+        fixture = TestBed.createComponent(GamePlayersListComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(GamePlayersListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
