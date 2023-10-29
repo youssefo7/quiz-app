44f4202429515c18ca58f1908e8505bdf8dbc937
Bryan-Tad <bryan.tadie@polymtl.ca>
2023-10-29 22:23:11 +0000

    Popup d'avertissement lors d'une tentiative de quitter la page de création quiz.

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { mapToCanActivate, RouterModule, Routes } from '@angular/router';
 import { AdminPageComponent } from '@app/pages/admin-page/admin-page.component';
-import { CreateEditQuizPageComponent } from '@app/pages/create-edit-quiz-page/create-edit-quiz-page.component';
+import { CreateEditQuizPageComponent, exitCreateEditQuizPageGuard } from '@app/pages/create-edit-quiz-page/create-edit-quiz-page.component';
 import { CreateGamePageComponent } from '@app/pages/create-game-page/create-game-page.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { HostGamePageComponent } from '@app/pages/host-game-page/host-game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { WaitingPageComponent } from '@app/pages/waiting-page/waiting-page.component';
 import { AdminGuardService } from '@app/services/admin-guard.service';
-
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainPageComponent },
@@ -19,6 +18,8 @@ const routes: Routes = [
     { path: 'game/:id/host', component: HostGamePageComponent },
     { path: 'quiz/new', component: CreateEditQuizPageComponent },
     { path: 'quiz/:id', component: CreateEditQuizPageComponent },
+    { path: 'quiz/new', component: CreateEditQuizPageComponent, canDeactivate: [exitCreateEditQuizPageGuard] },
+    { path: 'quiz/:id', component: CreateEditQuizPageComponent, canDeactivate: [exitCreateEditQuizPageGuard] },
     { path: 'waiting', component: WaitingPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
