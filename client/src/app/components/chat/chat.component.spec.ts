52a5e8891a6ffba87991dbcbf15b2640c6318c99
Massimo Donato <massimo.donato@polymtl.ca>
2023-10-30 12:03:56 -0400

    Premiere version presque completer de la fonctionnalite du clavardage avec des tests manquantes est echouantes a resoudre

--- client/src/app/components/chat/chat.component.spec.ts
+++ client/src/app/components/chat/chat.component.spec.ts
@@ -1,22 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatIcon } from '@angular/material/icon';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { SocketClientService } from '@app/services/socket-client.service';
 import { ChatComponent } from './chat.component';
 
 describe('ChatComponent', () => {
     let component: ChatComponent;
+    let socketClientServiceMock: jasmine.SpyObj<SocketClientService>;
     let fixture: ComponentFixture<ChatComponent>;
+
+    beforeEach(() => {
+        socketClientServiceMock = jasmine.createSpyObj('SocketClientService', ['connect', 'disconnect', 'send', 'on']);
+    });
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [ChatComponent, MatIcon],
+            providers: [
+                { provide: SocketClientService, useValue: socketClientServiceMock },
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        snapshot: {
+                            paramMap: convertToParamMap({ room: 'roomid' }),
+                        },
+                    },
+                },
+            ],
         });
         fixture = TestBed.createComponent(ChatComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
-    });
-
-    it('should create', () => {
-        expect(component).toBeTruthy();
     });
 
     it('expandTextArea should set height to scrollHeight if scrollHeight is less than 150px', () => {
@@ -44,4 +58,23 @@ describe('ChatComponent', () => {
         expect(component.currentInputLength).toEqual(inputLength);
         expect(component.characterCounterDisplay).toBe('4 / 200');
     });
+
+    it('should send a message to a specific room on the server and reset roomMessage with a roomMessage event', () => {
+        const event = 'roomMessage';
+        const testRoomId = 'roomid';
+        const message = 'Test Message';
+        component.roomMessage = message;
+        component.sendMessageToRoom();
+        expect(socketClientServiceMock.send).toHaveBeenCalledWith(event, { roomId: testRoomId, message });
+        expect(component.roomMessage).toEqual('');
+        expect(component.configureChatSocketFeatures()).toHaveBeenCalled();
+    });
+
+    // it('should add a message to roomMessages array from server on roomMessage event', () => {
+    //     const dataInfo = { name: 'Test name', time: '11:12:13', message: 'Test Message', sentByYou: false };
+    //     const roomMessage = 'message 1';
+    //     socketHelper.peerSideEmit('newRoomMessage', (data: dataInfo));
+    //     expect(component.roomMessages.length).toBe(1);
+    //     expect(component.roomMessages).toContain(roomMessage);
+    // });
 });
---
+++ client/src/app/interfaces/chat-message.ts
@@ -0,0 +1,6 @@
+export interface ChatMessage {
+    name: string;
+    time: string;
+    message: string;
+    sentByYou: boolean;
+}
