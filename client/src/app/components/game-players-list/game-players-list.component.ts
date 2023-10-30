5b76db5ab775de1041928c277e8d5b77d95bcbda
Youssef Ouarad <youssef.ouarad@polymtl.ca>
2023-10-29 20:44:35 -0400

    traitement dynamique de la liste des joueurs et de l'attribut has abandonned

--- client/src/app/components/game-players-list/game-players-list.component.ts
+++ client/src/app/components/game-players-list/game-players-list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { PlayerInfo } from '@app/interfaces/player-info';
+import { SocketClientService } from './../../services/socket-client.service';
 
 @Component({
     selector: 'app-game-players-list',
@@ -7,6 +8,15 @@ import { PlayerInfo } from '@app/interfaces/player-info';
     styleUrls: ['./game-players-list.component.scss'],
 })
 export class GamePlayersListComponent {
-    playersList: PlayerInfo[];
-    // TODO: ngOnInit should fetch the list of players to be used in the html (maybe given through Input)
+    @Input() playersList: PlayerInfo[];
+    socketService: SocketClientService;
+
+    configureBaseSocketFeatures() {
+        this.socketService.on('abandonedGame', (playerName) => {
+            const playerToUpdate = this.playersList.find((player) => player.name === playerName);
+            if (playerToUpdate) {
+                playerToUpdate.hasAbandoned = true;
+            }
+        });
+    }
 }
