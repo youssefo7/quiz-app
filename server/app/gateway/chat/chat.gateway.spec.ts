2d679f62b0b76ca0e1f25dfd62f3be8d7c1f8fe1
Massimo Donato <massimo.donato@polymtl.ca>
2023-10-31 16:07:58 -0400

    Tests pour le ChatGateway

--- server/app/gateway/chat/chat.gateway.spec.ts
+++ server/app/gateway/chat/chat.gateway.spec.ts
@@ -1,21 +1,68 @@
 import { RoomManagerService } from '@app/services/room-manager/room-manager.service';
+import { Logger } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { SinonStubbedInstance, createStubInstance, match, stub } from 'sinon';
+import { BroadcastOperator, Server, Socket } from 'socket.io';
 import { ChatGateway } from './chat.gateway';
 
 describe('ChatGateway', () => {
     let gateway: ChatGateway;
-    // let roomManagerServiceMock: RoomManagerService;
+    let logger: SinonStubbedInstance<Logger>;
+    let socket: SinonStubbedInstance<Socket>;
+    let server: SinonStubbedInstance<Server>;
+    let roomManagerServiceMock: RoomManagerService;
 
     beforeEach(async () => {
+        logger = createStubInstance(Logger);
+        socket = createStubInstance<Socket>(Socket);
+        server = createStubInstance<Server>(Server);
+
         const module: TestingModule = await Test.createTestingModule({
-            providers: [ChatGateway, RoomManagerService],
+            providers: [
+                ChatGateway,
+                {
+                    provide: Logger,
+                    useValue: logger,
+                },
+                RoomManagerService,
+            ],
         }).compile();
 
         gateway = module.get<ChatGateway>(ChatGateway);
-        // roomManagerServiceMock = module.get<RoomManagerService>(RoomManagerService);
+        roomManagerServiceMock = module.get<RoomManagerService>(RoomManagerService);
+        roomManagerServiceMock.rooms.push({
+            id: 'testId',
+            quizId: '1',
+            organizer: { socketId: 'organizerId', name: 'Organisateur' },
+            players: [
+                { socketId: 'playerId1', name: 'name1', points: 50, bonusCount: 0 },
+                { socketId: 'playerId2', name: 'name2', points: 200, bonusCount: 1 },
+            ],
+            isLocked: false,
+            bannedNames: [],
+            answerTimes: [],
+        });
+        gateway['server'] = server;
     });
 
     it('should be defined', () => {
         expect(gateway).toBeDefined();
+    });
+
+    it('handleRoomMessage() should send message if socket in the room', () => {
+        stub(socket, 'rooms').value(new Set(['testId']));
+        socket.to.returns({
+            emit: (event: string) => {
+                expect(event).toEqual('newRoomMessage');
+            },
+        } as BroadcastOperator<unknown, unknown>);
+        gateway.handleRoomMessage(socket, { roomId: 'testId', message: 'Test Message' });
+        expect(socket.emit.calledWith('sentByYou', match.object)).toBeTruthy();
+    });
+
+    it('handleRoomMessage() should not send message if socket not in the room', () => {
+        stub(socket, 'rooms').value(new Set());
+        gateway.handleRoomMessage(socket, { roomId: 'testId', message: 'Test Message' });
+        expect(server.to.called).toBeFalsy();
     });
 });
