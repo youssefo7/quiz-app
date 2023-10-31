eee228090061e3c183a1f32353407cbcaa8f1abf
Youssef Ouarad <youssef.ouarad@polymtl.ca>
2023-10-31 16:14:20 -0400

    ajout des tests pour liste des joueurs dans vue organisateur

--- client/src/app/components/game-players-list/game-players-list.component.spec.ts
+++ client/src/app/components/game-players-list/game-players-list.component.spec.ts
@@ -1,14 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { PlayerInfo } from '@app/interfaces/player-info';
+import { SocketClientService } from '@app/services/socket-client.service';
 import { GamePlayersListComponent } from './game-players-list.component';
 
 describe('GamePlayersListComponent', () => {
     let component: GamePlayersListComponent;
     let fixture: ComponentFixture<GamePlayersListComponent>;
+    let clientSocketService: jasmine.SpyObj<SocketClientService>;
+
+    const playersList: PlayerInfo[] = [
+        { name: 'Marc', hasAbandoned: false, score: 0 },
+        { name: 'Liam', hasAbandoned: false, score: 0 },
+    ];
 
     beforeEach(() => {
+        clientSocketService = jasmine.createSpyObj('SocketClientService', ['on']);
         TestBed.configureTestingModule({
             declarations: [GamePlayersListComponent],
+            providers: [
+                {
+                    provide: SocketClientService,
+                    useValue: clientSocketService,
+                },
+            ],
         });
         fixture = TestBed.createComponent(GamePlayersListComponent);
         component = fixture.componentInstance;
@@ -17,5 +31,46 @@ describe('GamePlayersListComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should update player status when abandonedGame event is received', () => {
+        const playerName = 'Marc';
+
+        component.playersList = playersList;
+
+        clientSocketService.on.and.callFake((eventName) => {
+            if (eventName === 'abandonedGame') {
+                component.updatePlayerStatus(playerName);
+            }
+        });
+
+        clientSocketService.on.calls.reset();
+        component.listenToSocketEvents();
+
+        expect(component.playersList[0].hasAbandoned).toBe(true);
+        expect(component.playersList[1].hasAbandoned).toBe(false);
+    });
+
+    it('should update player status when abandonedGame event is received', () => {
+        const response = {
+            pointsToAdd: 10,
+            name: 'Marc',
+        };
+        const newScore = 10;
+        const oldScore = 0;
+
+        component.playersList = playersList;
+
+        clientSocketService.on.and.callFake((eventName) => {
+            if (eventName === 'addPointsToPlayer') {
+                component.updatePayerScore(response);
+            }
+        });
+
+        clientSocketService.on.calls.reset();
+        component.listenToSocketEvents();
+
+        expect(component.playersList[0].score).toBe(newScore);
+        expect(component.playersList[1].score).toBe(oldScore);
     });
 });
--- client/src/app/components/game-players-list/game-players-list.component.ts
+++ client/src/app/components/game-players-list/game-players-list.component.ts
@@ -2,6 +2,11 @@ import { Component, Input, OnInit } from '@angular/core';
 import { PlayerInfo } from '@app/interfaces/player-info';
 import { SocketClientService } from './../../services/socket-client.service';
 
+interface AddPointsResponse {
+    pointsToAdd: number;
+    name: string;
+}
+
 @Component({
     selector: 'app-game-players-list',
     templateUrl: './game-players-list.component.html',
@@ -13,15 +18,30 @@ export class GamePlayersListComponent implements OnInit {
     constructor(public socketService: SocketClientService) {}
 
     ngOnInit(): void {
-        this.configureBaseSocketFeatures();
+        this.listenToSocketEvents();
     }
 
-    configureBaseSocketFeatures() {
-        this.socketService.on('abandonedGame', (playerName) => {
-            const playerToUpdate = this.playersList.find((player) => player.name === playerName);
-            if (playerToUpdate) {
-                playerToUpdate.hasAbandoned = true;
-            }
+    listenToSocketEvents() {
+        this.socketService.on('abandonedGame', (playerName: string) => {
+            this.updatePlayerStatus(playerName);
         });
+
+        this.socketService.on('addPointsToPlayer', (response: AddPointsResponse) => {
+            this.updatePayerScore(response);
+        });
+    }
+
+    updatePlayerStatus(playerName: string) {
+        const playerToUpdate = this.playersList.find((player) => player.name === playerName);
+        if (playerToUpdate) {
+            playerToUpdate.hasAbandoned = true;
+        }
+    }
+
+    updatePayerScore(response: AddPointsResponse) {
+        const playerToUpdate = this.playersList.find((player) => player.name === response.name);
+        if (playerToUpdate) {
+            playerToUpdate.score += response.pointsToAdd;
+        }
     }
 }
