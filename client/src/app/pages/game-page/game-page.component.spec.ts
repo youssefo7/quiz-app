c3d09f4437eacf22b0e2e12fab1340196cf830b6
Massimo Donato <massimo.donato@polymtl.ca>
2023-10-31 00:05:38 -0400

    Tests de clavardage 100%, un peu de correction a ajouter pour les tests dans GamePageComponent

--- client/src/app/pages/game-page/game-page.component.spec.ts
+++ client/src/app/pages/game-page/game-page.component.spec.ts
@@ -11,6 +11,7 @@ import { TopBarComponent } from '@app/components/top-bar/top-bar.component';
 import { PopupMessageConfig } from '@app/interfaces/popup-message-config';
 import { CommunicationService } from '@app/services/communication.service';
 import { GameService } from '@app/services/game.service';
+import { SocketClientService } from '@app/services/socket-client.service';
 import { of } from 'rxjs';
 import { GamePageComponent } from './game-page.component';
 import SpyObj = jasmine.SpyObj;
@@ -28,6 +29,7 @@ describe('GamePageComponent in test game route', () => {
     let communicationServiceMock: jasmine.SpyObj<CommunicationService>;
     let mockDialog: SpyObj<MatDialog>;
     let mockDialogRef: SpyObj<MatDialogRef<PopupMessageComponent>>;
+    let socketClientServiceMock: SocketClientServiceMock;
     let router: Router;
     let gameService: GameService;
     const mockedQuiz = {
@@ -61,6 +63,7 @@ describe('GamePageComponent in test game route', () => {
                 MatIcon,
             ],
             providers: [
+                { provide: SocketClientService, useValue: socketClientServiceMock },
                 { provide: ActivatedRoute, useValue: { snapshot: { paramMap: { get: () => '123' }, url: [{ path: 'test' }] } } },
                 { provide: MatDialog, useValue: mockDialog },
                 { provide: MatDialogRef, useValue: mockDialogRef },
@@ -140,6 +143,7 @@ describe('GamePageComponent in regular game route', () => {
     let fixture: ComponentFixture<GamePageComponent>;
     let router: Router;
     let communicationServiceMock: jasmine.SpyObj<CommunicationService>;
+    let socketClientServiceMock: SocketClientServiceMock;
 
     beforeEach(() => {
         communicationServiceMock = jasmine.createSpyObj('CommunicationService', ['getQuiz']);
@@ -158,6 +162,7 @@ describe('GamePageComponent in regular game route', () => {
             ],
             imports: [MatDialogModule],
             providers: [
+                { provide: SocketClientServiceMock, useValue: socketClientServiceMock },
                 { provide: ActivatedRoute, useValue: { snapshot: { paramMap: { get: () => '123' }, url: [{ path: '' }] } } },
                 { provide: CommunicationService, useValue: communicationServiceMock },
             ],
