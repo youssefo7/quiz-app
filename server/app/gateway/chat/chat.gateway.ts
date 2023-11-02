aa35dfb4f4627eba7db2dde0781a4cde4169dd4d
Massimo Donato <massimo.donato@polymtl.ca>
2023-11-01 20:51:29 -0400

    Correction des commentaires

--- server/app/gateway/chat/chat.gateway.ts
+++ server/app/gateway/chat/chat.gateway.ts
@@ -1,6 +1,5 @@
-import { Room } from '@app/interfaces/room';
 import { RoomManagerService } from '@app/services/room-manager/room-manager.service';
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { ChatEvents } from './chat.gateway.events';
@@ -9,14 +8,8 @@ import { ChatEvents } from './chat.gateway.events';
 @Injectable()
 export class ChatGateway {
     @WebSocketServer() private server: Server;
-    private rooms: Room[];
 
-    constructor(
-        private readonly logger: Logger,
-        private roomManager: RoomManagerService,
-    ) {
-        this.rooms = roomManager.rooms as Room[];
-    }
+    constructor(private roomManager: RoomManagerService) {}
 
     @SubscribeMessage(ChatEvents.RoomMessage)
     handleRoomMessage(socket: Socket, data: { roomId: string; message: string }) {
@@ -24,9 +17,9 @@ export class ChatGateway {
         const user = this.roomManager.findUser(socket.id, room);
         const time = new Date();
         const timeString = time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds();
-        if (socket.rooms.has(data.roomId)) {
-            socket.to(data.roomId).emit(ChatEvents.NewRoomMessage, { name: user.name, timeString, message: data.message, sentByUser: false });
-            socket.emit(ChatEvents.SentByYou, { name: user.name, timeString, message: data.message, sentByYou: true });
-        }
+        let messageData = { name: user.name, timeString, message: data.message, sentByUser: false };
+
+        socket.to(data.roomId).emit('newRoomMessage', messageData);
+        socket.emit('sentByYou', (messageData = { ...messageData, sentByUser: true }));
     }
 }
