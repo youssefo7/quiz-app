b3e998ac5bcf696bf0ddc989314790d2778391c7
youssefo7 <youssef.ouarad@polymtl.ca>
2023-11-02 01:28:24 +0000

    ajout de la liste de joueurs et de ses tests

--- client/src/app/components/game-players-list/game-players-list.component.spec.ts
+++ client/src/app/components/game-players-list/game-players-list.component.spec.ts
@@ -1,21 +1,84 @@
+import { GameEvents } from './../../../../../server/app/gateway/game/game.gateway.events';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { PlayerInfo } from '@app/interfaces/player-info';
+import { SocketClientService } from '@app/services/socket-client.service';
 import { GamePlayersListComponent } from './game-players-list.component';
 
 describe('GamePlayersListComponent', () => {
     let component: GamePlayersListComponent;
     let fixture: ComponentFixture<GamePlayersListComponent>;
+    let clientSocketServiceMock: jasmine.SpyObj<SocketClientService>;
+
+    const playersList: PlayerInfo[] = [
+        { name: 'Marc', hasAbandoned: false, score: 0 },
+        { name: 'Liam', hasAbandoned: false, score: 0 },
+    ];
+
+    const response = {
+        pointsToAdd: 10,
+        name: 'Marc',
+    };
 
     beforeEach(() => {
+        clientSocketServiceMock = jasmine.createSpyObj('SocketClientService', ['on']);
         TestBed.configureTestingModule({
             declarations: [GamePlayersListComponent],
+            providers: [
+                {
+                    provide: SocketClientService,
+                    useValue: clientSocketServiceMock,
+                },
+            ],
         });
         fixture = TestBed.createComponent(GamePlayersListComponent);
         component = fixture.componentInstance;
+        component.playersList = playersList;
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should call "updatePlayerScore" when "addPointsToPlayer" event is received', () => {
+        const updatePayerScoreSpy = spyOn(component, 'updatePlayerScore');
+
+        const updateScoreCallback = clientSocketServiceMock.on.calls.mostRecent().args[1];
+        updateScoreCallback(response);
+
+        expect(updatePayerScoreSpy).toHaveBeenCalledWith(response);
+    });
+
+    it('should update player status when abandonedGame event is received', () => {
+        const playerName = 'Marc';
+
+        clientSocketServiceMock.on.and.callFake((eventName) => {
+            if (eventName === GameEvents.PlayerAbandonedGame) {
+                component.updatePlayerStatus(playerName);
+            }
+        });
+
+        clientSocketServiceMock.on.calls.reset();
+        component.listenToSocketEvents();
+
+        expect(component.playersList[0].hasAbandoned).toBe(true);
+        expect(component.playersList[1].hasAbandoned).toBe(false);
+    });
+
+    it('should update player score when addPlayerPoints event is received', () => {
+        const newScore = 10;
+        const oldScore = 0;
+
+        clientSocketServiceMock.on.and.callFake((eventName) => {
+            if (eventName === GameEvents.AddPointsToPlayer) {
+                component.updatePlayerScore(response);
+            }
+        });
+
+        clientSocketServiceMock.on.calls.reset();
+        component.listenToSocketEvents();
+
+        expect(component.playersList[0].score).toBe(newScore);
+        expect(component.playersList[1].score).toBe(oldScore);
     });
 });
--- client/src/app/components/game-players-list/game-players-list.component.ts
+++ client/src/app/components/game-players-list/game-players-list.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PlayerInfo } from '@app/interfaces/player-info';
+import { GameEvents } from './../../../../../server/app/gateway/game/game.gateway.events';
+import { SocketClientService } from './../../services/socket-client.service';
+
+interface AddPointsResponse {
+    pointsToAdd: number;
+    name: string;
+}
 
 @Component({
     selector: 'app-game-players-list',
     templateUrl: './game-players-list.component.html',
     styleUrls: ['./game-players-list.component.scss'],
 })
-export class GamePlayersListComponent {
+export class GamePlayersListComponent implements OnInit {
     playersList: PlayerInfo[];
-    // TODO: ngOnInit should fetch the list of players to be used in the html (maybe given through Input)
+
+    constructor(public socketService: SocketClientService) {}
+
+    ngOnInit() {
+        this.listenToSocketEvents();
+        // TODO : fetch player list from back end
+    }
+
+    listenToSocketEvents() {
+        this.socketService.on(GameEvents.PlayerAbandonedGame, (playerName: string) => {
+            this.updatePlayerStatus(playerName);
+        });
+
+        this.socketService.on(GameEvents.AddPointsToPlayer, (response: AddPointsResponse) => {
+            this.updatePlayerScore(response);
+        });
+    }
+
+    updatePlayerStatus(playerName: string) {
+        const playerToUpdate = this.playersList.find((player) => player.name === playerName);
+        if (playerToUpdate) {
+            playerToUpdate.hasAbandoned = true;
+        }
+    }
+
+    updatePlayerScore(response: AddPointsResponse) {
+        const playerToUpdate = this.playersList.find((player) => player.name === response.name);
+        if (playerToUpdate) {
+            playerToUpdate.score += response.pointsToAdd;
+        }
+    }
 }
