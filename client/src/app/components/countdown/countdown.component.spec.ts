005f727bfd434f397e5019c13fa2cced6a3e456a
Gabrielle Côté <gaby_cote1@hotmail.ca>
2023-11-07 17:03:50 +0000

    Complétion des tests de la composante Countdown

--- client/src/app/components/countdown/countdown.component.spec.ts
+++ client/src/app/components/countdown/countdown.component.spec.ts
@@ -1,13 +1,24 @@
-// any is need to spy on private methods
+// On a besoin du any pour espionner sur les méthodes privées
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { MatIcon } from '@angular/material/icon';
 import { ActivatedRoute, Router } from '@angular/router';
+import { SocketTestHelper } from '@app/classes/socket-test-helper';
+import { GameEvents } from '@app/events/game.events';
+import { TimeEvents } from '@app/events/time.events';
 import { GameService } from '@app/services/game.service';
+import { SocketClientService } from '@app/services/socket-client.service';
 import { TimeService } from '@app/services/time.service';
 import { of } from 'rxjs';
+import { Socket } from 'socket.io-client';
 import { CountdownComponent } from './countdown.component';
 import SpyObj = jasmine.SpyObj;
+
+class MockSocketClientService extends SocketClientService {
+    override connect() {
+        // vide
+    }
+}
 
 describe('CountdownComponent', () => {
     let component: CountdownComponent;
@@ -15,6 +26,8 @@ describe('CountdownComponent', () => {
     let timeServiceMock: SpyObj<TimeService>;
     let routerMock: SpyObj<Router>;
     let gameServiceMock: SpyObj<GameService>;
+    let socketClientServiceMock: MockSocketClientService;
+    let socketHelper: SocketTestHelper;
 
     const mockQuiz = {
         $schema: 'quiz-schema.json',
@@ -45,9 +58,14 @@ describe('CountdownComponent', () => {
         timeServiceMock.getTime.and.returnValue(of(0));
         routerMock = jasmine.createSpyObj('Router', ['navigateByUrl']);
         gameServiceMock = jasmine.createSpyObj('GameService', ['setGameEndState', 'getQuizById']);
+        socketClientServiceMock = jasmine.createSpyObj('SocketClientService', ['on', 'connect', 'disconnect', 'socketExists', 'send']);
     });
 
     beforeEach(waitForAsync(() => {
+        socketHelper = new SocketTestHelper();
+        socketClientServiceMock = new MockSocketClientService();
+        socketClientServiceMock.socket = socketHelper as unknown as Socket;
+
         TestBed.configureTestingModule({
             declarations: [CountdownComponent, MatIcon],
             providers: [
@@ -55,6 +73,7 @@ describe('CountdownComponent', () => {
                 { provide: ActivatedRoute, useValue: { snapshot: { paramMap: { get: () => '123' }, url: [{ path: 'test' }] } } },
                 { provide: Router, useValue: routerMock },
                 { provide: GameService, useValue: gameServiceMock },
+                { provide: SocketClientService, useValue: socketClientServiceMock },
             ],
         }).compileComponents();
     }));
@@ -155,5 +174,72 @@ describe('CountdownComponent', () => {
         component.isHost = true;
         fixture.detectChanges();
         expect(fixture.nativeElement.querySelector('#countdown-options')).toBeTruthy();
+    });
+
+    it('should start socket timer to 3s with transitionClock', () => {
+        const roomId = '123';
+        const transitionTime = 3;
+        const oneSecondInterval = 1000;
+        component['isTestGame'] = false;
+        component['roomId'] = roomId;
+        const sendSpy = spyOn(component['socketClientService'], 'send');
+        component['transitionClock']();
+
+        expect(sendSpy).toHaveBeenCalledWith(TimeEvents.StartTimer, { initialTime: transitionTime, roomId, tickRate: oneSecondInterval });
+    });
+
+    it('should react to socket current timer event and update timer color', waitForAsync(() => {
+        const time = 15;
+        component['isTestGame'] = false;
+        const setClockColorToRedSpy = spyOn<any>(component, 'setClockColorToRed');
+
+        component['reactToTimerEvent']();
+        socketHelper.peerSideEmit(TimeEvents.CurrentTimer, time);
+        expect(component['socketTime']).toEqual(time);
+        expect(setClockColorToRedSpy).toHaveBeenCalled();
+    }));
+
+    it('should react to socket timer finished event and start next question when finished transition clock', () => {
+        const initialQuestionIndex = 0;
+        const questionClockSpy = spyOn<any>(component, 'questionClock');
+        component['hasFinishedTransitionClock'] = true;
+        component['currentQuestionIndex'] = initialQuestionIndex;
+
+        component['reactToTimerFinishedEvent']();
+        socketHelper.peerSideEmit(TimeEvents.TimerFinished);
+        expect(component['currentQuestionIndex']).toEqual(initialQuestionIndex + 1);
+        expect(component['hasFinishedTransitionClock']).toBeFalse();
+        expect(questionClockSpy).toHaveBeenCalled();
+    });
+
+    it('should react to socket timer finished event and emit transition clock finished when host', () => {
+        const roomId = '123';
+        component['hasFinishedTransitionClock'] = true;
+        component['roomId'] = roomId;
+        component['isHost'] = true;
+        const sendSpy = spyOn(component['socketClientService'], 'send');
+
+        component['reactToTimerFinishedEvent']();
+        socketHelper.peerSideEmit(TimeEvents.TimerFinished);
+        expect(sendSpy).toHaveBeenCalledWith(TimeEvents.TransitionClockFinished, roomId);
+    });
+
+    it('should set time to 0 when getting timer interrupted event', () => {
+        component['socketTime'] = 15;
+
+        component['reactToTimerInterruptedEvent']();
+        socketHelper.peerSideEmit(TimeEvents.TimerInterrupted);
+        expect(component['socketTime']).toEqual(0);
+    });
+
+    it('should start transition clock prepare next question when getting next question event', () => {
+        component['currentQuestionIndex'] = 0;
+        component['lastQuestionIndex'] = 3;
+        const transitionClockSpy = spyOn<any>(component, 'transitionClock');
+
+        component['reactToNextQuestionEvent']();
+        socketHelper.peerSideEmit(GameEvents.NextQuestion);
+        expect(component['hasFinishedTransitionClock']).toBeTrue();
+        expect(transitionClockSpy).toHaveBeenCalled();
     });
 });
