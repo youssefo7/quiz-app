5edb416327341c73e0cbb819f782d48fac52324b
Youssef Ouarad <youssef.ouarad@polymtl.ca>
2023-11-07 14:03:08 -0500

    fix affichage histograme

--- client/src/app/components/histogram/histogram.component.spec.ts
+++ client/src/app/components/histogram/histogram.component.spec.ts
@@ -1,10 +1,6 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { ActivatedRoute } from '@angular/router';
-import { GameService } from '@app/services/game.service';
 import { SocketClientService } from '@app/services/socket-client.service';
-import { TimeService } from '@app/services/time.service';
 import { NgChartsModule } from 'ng2-charts';
-import { of } from 'rxjs';
 import { HistogramComponent } from './histogram.component';
 
 import SpyObj = jasmine.SpyObj;
@@ -12,25 +8,19 @@ import SpyObj = jasmine.SpyObj;
 describe('HistogramComponent', () => {
     let component: HistogramComponent;
     let fixture: ComponentFixture<HistogramComponent>;
-    let gameServiceMock: SpyObj<GameService>;
-    let timeServiceMock: SpyObj<TimeService>;
     let socketClientServiceMock: SpyObj<SocketClientService>;
-    const mockedQuiz = {
-        $schema: 'test.json',
-        id: '123',
-        title: 'Test quiz',
-        description: 'Test quiz description',
-        visibility: true,
-        duration: 60,
-        lastModification: '2018-11-13T20:20:39+00:00',
-        questions: [],
-    };
+    // const mockedQuiz = {
+    //     $schema: 'test.json',
+    //     id: '123',
+    //     title: 'Test quiz',
+    //     description: 'Test quiz description',
+    //     visibility: true,
+    //     duration: 60,
+    //     lastModification: '2018-11-13T20:20:39+00:00',
+    //     questions: [],
+    // };
 
     beforeEach(() => {
-        gameServiceMock = jasmine.createSpyObj('GameService', ['getQuizById']);
-        gameServiceMock.getQuizById.and.returnValue(Promise.resolve(mockedQuiz));
-        timeServiceMock = jasmine.createSpyObj('TimeService', ['subscribeToGameService', 'getTime']);
-        timeServiceMock.getTime.and.returnValue(of(0));
         socketClientServiceMock = jasmine.createSpyObj('SocketClientService', ['on']);
     });
 
@@ -38,12 +28,7 @@ describe('HistogramComponent', () => {
         TestBed.configureTestingModule({
             declarations: [HistogramComponent],
             imports: [NgChartsModule],
-            providers: [
-                { provide: ActivatedRoute, useValue: { snapshot: { paramMap: { get: () => '123' } } } },
-                { provide: GameService, useValue: gameServiceMock },
-                { provide: TimeService, useValue: timeServiceMock },
-                { provide: SocketClientService, useValue: socketClientServiceMock },
-            ],
+            providers: [{ provide: SocketClientService, useValue: socketClientServiceMock }],
         }).compileComponents();
         fixture = TestBed.createComponent(HistogramComponent);
         component = fixture.componentInstance;
@@ -52,10 +37,5 @@ describe('HistogramComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
-    });
-
-    it('should fetch the quiz ', () => {
-        component['getQuiz']();
-        expect(gameServiceMock.getQuizById).toHaveBeenCalledWith(mockedQuiz.id);
     });
 });
