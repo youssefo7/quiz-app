5bcadc689dd0348f040bfd0d0dc1608f995552dc
Youssef Ouarad <youssef.ouarad@polymtl.ca>
2023-11-07 22:59:40 -0500

    tests pour results page

--- client/src/app/pages/results-page/results-page.component.spec.ts
+++ client/src/app/pages/results-page/results-page.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { MatIcon } from '@angular/material/icon';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ChatComponent } from '@app/components/chat/chat.component';
 import { GamePlayersListComponent } from '@app/components/game-players-list/game-players-list.component';
 import { TopBarComponent } from '@app/components/top-bar/top-bar.component';
@@ -16,6 +16,9 @@ describe('ResultsPageComponent', () => {
     let roomCommunicationServiceMock: jasmine.SpyObj<RoomCommunicationService>;
 
     beforeEach(() => {
+        const mockRouter = jasmine.createSpyObj('Router', ['navigate', 'navigateByUrl']);
+        mockRouter.navigateByUrl.and.returnValue(Promise.resolve(true));
+        Object.defineProperty(mockRouter, 'url', { get: () => '/host' });
         clientSocketServiceMock = jasmine.createSpyObj('SocketClientService', ['on', 'socketExists', 'connect', 'disconnect', 'send']);
         clientSocketServiceMock.socketExists.and.returnValue(true);
         roomCommunicationServiceMock = jasmine.createSpyObj('RoomCommunicationService', ['getRoomPlayers']);
@@ -26,6 +29,10 @@ describe('ResultsPageComponent', () => {
                 {
                     provide: SocketClientService,
                     useValue: clientSocketServiceMock,
+                },
+                {
+                    provide: Router,
+                    useValue: mockRouter,
                 },
                 { provide: RoomCommunicationService, useValue: roomCommunicationServiceMock },
                 { provide: ActivatedRoute, useValue: { snapshot: { paramMap: { get: () => '123' }, url: [{ path: 'results' }] } } },
@@ -38,5 +45,23 @@ describe('ResultsPageComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should initialize the component correctly if the user has an active socket or not', async () => {
+        clientSocketServiceMock.socketExists.and.returnValue(false);
+        await component.ngOnInit();
+        expect(clientSocketServiceMock.connect).toHaveBeenCalled();
+    });
+
+    it('should call handleNavigation on ngOnDestroy', () => {
+        const handleNavigationSpy = spyOn(component, 'handleNavigation');
+
+        component.ngOnDestroy();
+        expect(handleNavigationSpy).toHaveBeenCalled();
+    });
+
+    it('should call send and disconnect on handleNavigation if it is not the host', () => {
+        component.handleNavigation();
+        expect(clientSocketServiceMock.disconnect).toHaveBeenCalled();
     });
 });
