2a29b5323a242db2150d852d6225c10245a66656
youssefo7 <youssef.ouarad@polymtl.ca>
2023-11-28 20:53:47 +0000

    uniformisation du ui de create game page

--- client/src/app/pages/create-game-page/create-game-page.component.ts
+++ client/src/app/pages/create-game-page/create-game-page.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-create-game-page',
     templateUrl: './create-game-page.component.html',
-    styleUrls: ['./create-game-page.component.scss'],
+    styleUrls: ['./create-game-page.component.scss', '../../../assets/shared.scss'],
 })
 export class CreateGamePageComponent {}
